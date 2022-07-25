2a84d340f82c2b1af4c329adfd33692684f24a3e
Marina Gutkevich <gutmalina@mail.ru>
2022-07-25 19:27:43 +0300

    make schema & models

---
+++ models/movie.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {},
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {},
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {},
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
