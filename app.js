ffa7be8dd75bb5dcee04ad19b6057d757a43d6b8
Marina Gutkevich <gutmalina@mail.ru>
2022-07-28 12:33:05 +0300

    make cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/error');
+const cors = require('./middlewares/cors');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -15,6 +16,9 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/** обработка кросс-доменных запросов */
+app.use(cors);
 
 /** обработка логгер запросов */
 app.use(requestLogger);
---
+++ middlewares/cors.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'http://webdiploma.nomoredomains.xyz',
+  'https://webdiploma.nomoredomains.xyz',
+  'http://localhost:3000',
+  'https://localhost:3000',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).send();
+  }
+  next();
+  return true;
+};
