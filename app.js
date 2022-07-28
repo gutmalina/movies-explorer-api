e425a5293349cbc55cc6dc4be2e8dc14e69ea4bd
Marina Gutkevich <gutmalina@mail.ru>
2022-07-28 15:57:29 +0300

    make readme & security

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/error');
 const cors = require('./middlewares/cors');
 const routes = require('./routes/index');
+const { PORT_MONGO } = require('./utils/config');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+/** установка лимита на количестко запросов с одного IP, 15мин 100 запросов */
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+/** подключение лимита запросов */
+app.use(limiter);
+
 /** настройка заголовков для защиты приложения */
 app.use(helmet());
 
+/** преобразование тела запроса в json */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,11 +50,8 @@ app.use(error);
 
 /** подключение к mongo и серверу */
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/moviesdb');
-  app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Connect ${PORT}`);
-  });
+  await mongoose.connect(PORT_MONGO);
+  app.listen(PORT);
 }
 
 main();
---
+++ utils/config.js
@@ -0,0 +1,3 @@
+const PORT_MONGO = 'mongodb://127.0.0.1:27017/moviesdb';
+
+module.exports = { PORT_MONGO };
